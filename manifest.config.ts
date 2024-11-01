a5442710b4b08467c0bdc0696cdf3d615e8865fa
xuqingkai <xuqingkai@hd123.com>
2024-11-01 14:39:15 +0800

    chore: 🚀 调整项目的名字

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,7 +1,7 @@
 import { defineManifestConfig } from '@uni-helper/vite-plugin-uni-manifest'
 
 export default defineManifestConfig({
-  'name': 'vitesse-uni-app',
+  'name': 'wot-demo',
   'appid': '',
   'description': '',
   'versionName': '1.0.0',
