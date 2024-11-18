3da843ba33bf62d2a8032dabf3061b2ce87e46a9
不如摸鱼去 <1780903673@qq.com>
2024-11-18 15:20:54 +0800

    fix: 🐛 修复分包路由未注册的问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,34 @@
+/*
+ * @Author: weisheng
+ * @Date: 2024-11-18 15:17:38
+ * @LastEditTime: 2024-11-18 15:20:06
+ * @LastEditors: weisheng
+ * @Description:
+ * @FilePath: /wot-demo/src/router/index.ts
+ * 记得注释
+ */
 /// <reference types="@uni-helper/vite-plugin-uni-pages/client" />
-import { pages } from 'virtual:uni-pages'
+import { pages, subPackages } from 'virtual:uni-pages'
 
-console.log(pages)
+function generateRoutes() {
+  const routes = pages.map((page) => {
+    const newPath = `/${page.path}`
+    return { ...page, path: newPath }
+  })
+  if (subPackages && subPackages.length > 0) {
+    subPackages.forEach((subPackage) => {
+      const subRoutes = subPackage.pages.map((page: any) => {
+        const newPath = `/${subPackage.root}/${page.path}`
+        return { ...page, path: newPath }
+      })
+      routes.push(...subRoutes)
+    })
+  }
+  return routes
+}
 
 const router = createRouter({
-  routes: pages.map((page) => {
-    page.path = `/${page.path}`
-    return page
-  })
-  ,
+  routes: generateRoutes(),
 })
 router.beforeEach((to, from, next) => {
   console.log(to)
