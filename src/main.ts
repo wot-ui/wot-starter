85fe41c5cff3a696cbd82449a5cf7d576901ef1c
tnt group <dodu@live.cn>
2025-10-24 08:45:06 +0800

    feat: ✨ 使用 pinia-plugin-persistedstate 持久化 store

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import 'uno.css'
 import '@/style/index.scss'
 
-const pinia = createPinia()
-pinia.use(persistPlugin)
 export function createApp() {
   const app = createSSRApp(App)
   app.use(router)
-  app.use(pinia)
+  app.use(store)
   return {
     app,
   }
